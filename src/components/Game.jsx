fd04b0598f79ef8d3d74acf2a288a3edaea16312
Victor Chapman <victor.f.chapman@gmail.com>
2022-01-14 10:47:22 -0800

    worked on site

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,9 +2,20 @@ import { useState, useEffect } from "react";
 import "../App.css";
 
 export default function Game() {
-  const [answer, setAnswer] = useState("hello");
+  const [answer, setAnswer] = useState(["h", "e", "l", "l", "o"]);
   const [input, setInput] = useState([]);
+  const [something, setSomething] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    for (let i = 0; i < answer.length; i++) {
+      for (let j = 0; i < input.length; j++) {
+        if (answer[i] === input[j]) {
+          setSomething(true);
+        }
+      }
+    }
+  };
   const handleChange = (current) => {
     setInput([current.split("")]);
   };
@@ -12,15 +23,22 @@ export default function Game() {
   useEffect(() => {
     console.log(input);
   }, [input]);
+  useEffect(() => {
+    console.log(something);
+  }, [something]);
 
   return (
     <div>
-      <input
-        maxlength="5"
-        className="hello"
-        onChange={(e) => handleChange(e.target.value)}
-        type="text"
-      />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          maxLength="5"
+          className="hello"
+          onChange={(e) => handleChange(e.target.value)}
+          type="text"
+        />
+        <input type="submit" />
+      </form>
+      <p>{something}</p>
     </div>
   );
 }
